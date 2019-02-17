89e0c4ddde52aca11d00e2e80c728bf0f4390277
Vinicius Rufine <vsrufine@gmail.com>
2019-02-17 07:42:29 -0300

    props zzz

---
+++ m03/server.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
+const config = require('./webpack.config')
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  stats: {
+    colors: true
+  }
+}).listen(3000, err => {
+  if (err) {
+    return console.error(err)
+  }
+  console.log('Dev Server -> http://localhost:3000')
+})
---
+++ m03/src/app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+import React, { createClass } from 'react'
+
+import Title from './title'
+
+const App = createClass({
+  render: () => (
+    <div>
+      <Title name='João da Silva' />
+    </div>
+  )
+})
+
+export default App
